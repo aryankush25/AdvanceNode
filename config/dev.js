d5c000d56a273dccc594c3750484d46ac3ba9101
Aryan Agarwal <aryan@geekyants.com>
2021-07-20 03:51:00 +0530

    changes

--- config/dev.js
+++ config/dev.js
@@ -1,7 +1,7 @@
 module.exports = {
-  googleClientID:
-    '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
-  googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
-  cookieKey: '123123123',
-};
+	googleClientID: '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
+	googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
+	mongoURI:
+		'mongodb://aryankush25:OKtR8b9O5mn1SYQG@cluster0-shard-00-00.dcojo.mongodb.net:27017,cluster0-shard-00-01.dcojo.mongodb.net:27017,cluster0-shard-00-02.dcojo.mongodb.net:27017/<dbname>?ssl=true&replicaSet=atlas-gdbxdc-shard-0&authSource=admin&retryWrites=true&w=majority',
+	cookieKey: '123123123'
+}
